b26483228f55a4875ad5946f2b51567cfe00bb2d
hogaku <user.name>
2023-10-01 02:03:31 +0900

    Add: Separated code

---
+++ clip_utils/CLIPEmbedder.py
@@ -0,0 +1,20 @@
+
+from torch import nn
+
+class CLIPEmbedder(nn.Module):
+    """Uses the CLIP transformer encoder for text (from Hugging Face)"""
+    def __init__(self, version="openai/clip-vit-large-patch14", device="cuda"):
+        super().__init__()
+        from transformers import CLIPTextModel, logging
+        logging.set_verbosity_error()
+        self.transformer = CLIPTextModel.from_pretrained(version)
+        self.transformer = self.transformer.eval().requires_grad_(False).to(device)
+
+    @property
+    def device(self):
+        return self.transformer.device
+
+    def forward(self, tok_out):
+        input_ids, cross_cond_padding = tok_out
+        clip_out = self.transformer(input_ids=input_ids.to(self.device), output_hidden_states=True)
+        return clip_out.hidden_states[-1], cross_cond_padding.to(self.device), clip_out.pooler_output
